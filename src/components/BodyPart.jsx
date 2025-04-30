59aaada1b7994db3daacac297917fa50d705463b
ishank-kumar <145825143+ishank-kumar@users.noreply.github.com>
2025-04-30 23:44:16 +0530

    first commit

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -14,9 +14,10 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
       window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
     }}
   >
-    <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
+    <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }}/>
     <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
   </Stack>
 );
 
 export default BodyPart;
+
